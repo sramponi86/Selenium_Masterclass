9c599e9f60dcd111e95ad672ce3f9283f51e0842
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2023-01-22 16:41:41 +0100

    Add files via upload

---
+++ automationframework_parametrized_tests/src/test/java/SearchTests.java
@@ -0,0 +1,66 @@
+import drivers.DriverSingleton;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import pages.*;
+import utils.Constants;
+import utils.FrameworkProperties;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class SearchTests {
+
+    static FrameworkProperties frameworkProperties;
+    static WebDriver driver;
+    static HomePage homePage;
+    static SignInPage signInPage;
+    static CheckoutPage checkoutPage;
+    static String inputString;
+    static Boolean expectedResult;
+
+    @BeforeClass
+    public static void initializeObjects(){
+        frameworkProperties = new FrameworkProperties();
+        DriverSingleton.getInstance(frameworkProperties.getProperty(Constants.BROWSER));
+        driver = DriverSingleton.getDriver();
+        homePage = new HomePage();
+        signInPage = new SignInPage();
+        checkoutPage = new CheckoutPage();
+    }
+
+    public SearchTests(String inputString, Boolean expectedResult){
+        this.inputString = inputString;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Collection searchOptions(){
+        return Arrays.asList(new Object[][] {
+            {"DNK" , true},
+            {"Blouse" , false},
+            {"Dress" , false},
+            {"o" , false},
+            {"test" , false}
+        });
+    }
+
+    @Test
+    public void testingSearch(){
+        driver.get(Constants.URL_store);
+        homePage.clickStore();
+        assertEquals(expectedResult, homePage.searchElement(inputString));
+    }
+
+    @AfterClass
+    public static void closeObjects(){
+        driver.close();
+    }
+}
