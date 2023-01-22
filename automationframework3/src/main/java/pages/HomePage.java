491b37c8405fdc323a7f522dec9befe7e74474bc
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2023-01-22 15:31:08 +0100

    Add files via upload

--- automationframework3/src/main/java/pages/HomePage.java
+++ automationframework3/src/main/java/pages/HomePage.java
@@ -18,13 +18,13 @@ public class HomePage {
         PageFactory.initElements(driver, this);
     }
 
-    @FindBy(css = "#menu-item-368 > a")
+    @FindBy(id = "menu-item-440")
     private WebElement signInButton;
 
-    @FindBy(id = "menu-item-362")
+    @FindBy(id = "menu-item-430")
     private WebElement shopButton;
 
-    @FindBy(css = "#menu-item-371 > a")
+    @FindBy(id = "menu-item-443")
     private WebElement username;
 
     public void clickSignIn() {
@@ -40,6 +40,8 @@ public class HomePage {
     }
 
     public String getUsername() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.elementToBeClickable(username));
         return username.getText();
     }
 
