0169abc706229cae79293d69e01a917ca232c68b
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2023-01-22 16:41:17 +0100

    Add files via upload

--- automationframework_parametrized_tests/src/main/java/pages/HomePage.java
+++ automationframework_parametrized_tests/src/main/java/pages/HomePage.java
@@ -18,14 +18,26 @@ public class HomePage {
         PageFactory.initElements(driver, this);
     }
 
-    @FindBy(css = "#menu-item-368 > a")
+    @FindBy(id = "menu-item-440")
     private WebElement signInButton;
 
-    @FindBy(id = "menu-item-362")
+    @FindBy(id = "menu-item-430")
     private WebElement shopButton;
 
-    @FindBy(css = "#menu-item-371 > a")
+    @FindBy(id = "menu-item-443")
     private WebElement username;
+
+    @FindBy(id = "menu-item-45")
+    private WebElement storeButton;
+
+    @FindBy(id = "woocommerce-product-search-field-0")
+    private WebElement searchField;
+
+    @FindBy(css = "#woocommerce_product_search-4 > form > button")
+    private WebElement searchButton;
+
+    @FindBy(css = "#main > div > ul > li.ast-col-sm-12.ast-article-post.astra-woo-hover-swap.product.type-product.post-2726.status-publish.first.instock.product_cat-men.product_cat-shoes.has-post-thumbnail.featured.shipping-taxable.purchasable.product-type-simple > div.astra-shop-summary-wrap > a > h2")
+    private WebElement searchResults;
 
     public void clickSignIn() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
@@ -40,7 +52,31 @@ public class HomePage {
     }
 
     public String getUsername() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.elementToBeClickable(username));
         return username.getText();
+    }
+
+    public void clickStore() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.elementToBeClickable(storeButton));
+        storeButton.click();
+    }
+
+    public Boolean searchElement(String value) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.elementToBeClickable(searchField));
+        searchField.sendKeys(value);
+        searchButton.click();
+        try {
+            if(searchResults.isEnabled())
+                return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
+        return false;
     }
 
 }
