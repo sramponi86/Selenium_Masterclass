07235e9b15fb032957250f6865d3e9ec81f9553c
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2023-01-30 10:00:29 +0100

    Add files via upload

---
+++ rest_assured/src/main/java/Constants.java
@@ -0,0 +1,19 @@
+public class Constants {
+    public static final String ID = "id";
+    public static final String USERNAME = "userName";
+    public static final String FIRST_NAME = "firstName";
+    public static final String LAST_NAME = "lastName";
+    public static final String EMAIL = "emailAddress";
+    public static final String COUNTRY_OF_RESIDENCE = "countryOfResidence";
+    public static final String PHONE_NUMBER = "phoneNumber";
+    public static final String ADDRESS = "address";
+    public static final String DESCRIPTION = "description";
+    public static final String PASSWORD = "password";
+    public static final String BASE_URL = "http://localhost:8080/";
+    public static final String SIGN_UP_URL = "api/signup";
+    public static final String RETRIEVE_URL = "api/users";
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String CONTENT_TYPE_VALUE = "application/json";
+    public static final int SUCCESS_CODE = 200;
+    public static final String SEPARATOR = "/";
+}
